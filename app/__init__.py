f1fe7b2fd85c2d1914456e9d9657efd27af91a22
Amirth24 <amirth300324@gmail.com>
2023-04-09 19:05:11 +0530

    Saved the uploaded files in servers

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,11 @@ import os
 import uuid
 import json
 
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, current_app
 from flask_bootstrap import Bootstrap
 
-from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg','zip'}
-def allowed_file(filenames):
-    t = []
-    for filename in filenames:
-        t.append('.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
-
-    return all(t)
+from app.utils import handle_file
 
 bootstrap = Bootstrap()
 
@@ -30,21 +23,36 @@ def create_app():
 
 
     app.template_folder = 'templates'
-    app.config['UPLOAD_FOLDER'] = '/uploads'
+    app.config['UPLOAD_FOLDER'] = os.path.abspath('upload')
 
+    if not os.path.exists(app.config['UPLOAD_FOLDER'])  :
+        app.logger.info('Upload Folder Created')
+        os.mkdir(os.path.abspath('upload'))
 
-
-    app.config.from_file('config.json', load=json.load)
-
+    # app.config.from_file('config.json', load=json.load)
 
     @app.route('/')
     def index():
-        session['username'] = uuid.uuid1()
+        userid = session['userid']
+        if not userid:
+            userid = uuid.uuid1()
+            app.logger.info('Creating user', userid)
+            session['userid'] = userid
+
+
+        
+        if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], str(userid))):
+            os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], str(userid)))    
+            app.logger.info('Folder for '+str(userid)+' created.')
+
+
+
+
 
         return render_template('index.html'), 200
     
 
-    @app.route('/upload', methods=['POST'])
+    @app.route('/upload', methods=['GET','POST'])
     def upload():
         if 'files' not in request.files :
             flash('No file part')
@@ -52,21 +60,22 @@ def create_app():
         
         
         files = request.files.getlist('files')
-        print(files)
+        
+        
 
-        if files== []:
+        if files[0].filename == '':
             flash('No file selected')
             return redirect('/')
         
         # if files and allowed_file(files.filename):
-        
+        for file_ in files:
+            st  = handle_file(file_)
+            if not st : 
+                return redirect('/')
 
        
         return render_template('upload.html')
     
-    @app.route('/upload/' , methods=['GET'])
-    def upload_get():
 
-        return 'Files uploaded sucessfully'
 
     return app
